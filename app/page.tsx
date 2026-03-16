2ac689f6ef0d34eabd0102500b2948351e0dcefe
Anand <anandmurti221b.com>
2026-03-17 00:52:34 +0530

    kew;

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,55 @@
-export default function Home() {
-  return (<> 
-  <h1>w h at is de deal</h1>
-  <h1>45354365475685668</h1>
-</>);
+"use client"
+
+import React, { useState, ChangeEvent, FormEvent } from "react";
+
+interface FormData {
+  name: string;
+  strength: string;
 }
+
+function App() {
+  const [formData, setFormData] = useState<FormData>({ name: "", strength: "" });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <h1>
+        {formData.name} has {formData.strength}
+      </h1>
+      <h2>Simple Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name: </label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Strength: </label>
+          <input
+            type="text"
+            name="strength"
+            value={formData.strength}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+
+      <h3>Output:</h3>
+      <p>Name: {formData.name}</p>
+      <p>Strength: {formData.strength}</p>
+    </div>
+  );
+}
+
+export default App;
