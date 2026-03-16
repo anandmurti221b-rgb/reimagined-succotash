e2fc42531b610a52efbf819cb3df5910f1a30f8f
Anand <anandmurti221b.com>
2026-03-16 16:43:12 +0530

    somesome

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 export default function Home() {
   return (<> 
   <h1>Welcome to My Next.js App!</h1>
-  <h1>nono juko nuko guko</h1>
-  <h1>bobo</h1>
-  <h1>toto</h1>
-  <h1>go ho yo to ho mp</h1>
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque, nemo. Non repellendus quas architecto id recusandae quasi alias vel ducimus adipisci. Laborum sequi ipsa veritatis corrupti pariatur sed atque. Laborum!</p>
   </>);
 }
